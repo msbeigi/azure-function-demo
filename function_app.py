8c972207a6ec2d2751e4334716d5d2b247b7bc96
Mahsa Beigi <msbeigi@gmail.com>
2024-01-19 16:52:33 +0000

    Change app fucntion

--- function_app.py
+++ function_app.py
@@ -10,21 +10,31 @@ app = func.FunctionApp()
 
 @app.route(route="HttpSampleProcessor", auth_level=func.AuthLevel.ANONYMOUS)
 def HttpSampleProcessor(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    # logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-    else:
+    # name = req.params.get('name')
+    # if not name:
+    #     try:
+    #         req_body = req.get_json()
+    #     except ValueError:
+    #         pass
+    # else:
+    #     name = req_body.get('name')
+
+    # if name:
+    #     return func.HttpResponse(f"\nHello, {name}. This HTTP triggered function executed successfully.")
+    # else:
+    #     return func.HttpResponse(
+    #          "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+    #          status_code=200
+    #     )
+    try:
+        req_body = req.get_json()
         name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"\nHello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        if name:
+                return func.HttpResponse(json.dumps({"message": f"Hello, {name}!"}), mimetype="application/json")
+        else:
+                return func.HttpResponse("Invalid input. 'name' parameter is required.", status_code=400)
+    except ValueError:
+        return func.HttpResponse("Invalid JSON input.", status_code=400)
